003ccd1e811e39e79cdbabb8925dd8a72f654895
CanKRVN <erdacan54@gmail.com>
2020-09-12 12:32:59 +0300

    first commit
    added basic mechanics: throwing ball, camera movement, ball counter,  ball prefab,  gameover.

---
+++ Assets/BallScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScore : MonoBehaviour
+{
+    public float ballCount = CameraCharacter.ballCount;
+    public GameObject hitobstacle;
+    private void Start()
+    {
+        
+    }
+    private float BallCollect(Collider other)
+    {
+        if (other.gameObject.CompareTag("hitobstacle"))
+        {
+            Destroy(other.gameObject);
+            ballCount ++;
+            Debug.Log("+3 ball");
+        }
+        return ballCount;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BallCollect(other);
+    }
+
+}
---
+++ Assets/Scripts/CameraCharacter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CameraCharacter : MonoBehaviour
+{
+    public float spawnHelper = 4.5f;
+    public GameObject ball;
+    public float ballForce;
+    public float camSpeed;
+
+    private Camera _cam;
+
+    public static float ballCount = 15;
+    public Text BallCountText;
+    public Text GameOverText;
+
+    void Start()
+    {
+        _cam = GetComponent<Camera>();
+
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 1 * camSpeed);
+
+        Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && ballCount > 0)
+        {
+            GameObject ballRigid;
+            ballRigid = Instantiate(ball, transform.position, transform.rotation) as GameObject;
+            ballCount--;
+            Vector3 targetLoc = ray.direction;
+            ballRigid.GetComponent<Rigidbody>().AddForce(targetLoc * ballForce);
+        }
+
+        BallCountText.text = " " + ballCount;
+
+        if (ballCount == 0)
+        {
+            GameOverText.gameObject.SetActive(true);
+            camSpeed = 0;
+            
+        }
+
+       // Debug.Log(Time.time);
+    }
+}
